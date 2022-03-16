feb8b80ba97cbe6cd28e4d0d0e7b86e76f347b09
Alex Anacleto Amorim <alex.amorim95@gmail.com>
2022-03-16 16:46:20 -0300

    Primeiro Commit Intellij

---
+++ src/br/com/DIO/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.DIO;
+
+import br.com.DIO.Model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+    }
+}
